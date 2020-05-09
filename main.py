7ffacb3971ac9db59b9e39dfe738af1c27e6c94c
Thomas Martin <thomas.hennoo@hotmail.com>
2020-05-10 01:41:25 +0200

    Update main.py
    Added docstrings and comments.
    Made the columns list auto generate from the csv file

--- main.py
+++ main.py
@@ -44,25 +44,39 @@ class Member():
 
     def set_attacking_th_difference(self, th=int):
         """
+        The th difference between this member and the
+        person he raided
         th is positive int if th is higher
         th is negative int if th is lower
         """
         self.attacking_th_difference = th
 
     def set_attack_stars(self, stars=int):
+        """
+        Define the stars gained from attacking
+        """
         self.attack_stars = stars
 
     def set_defending_th_difference(self, th=int):
         """
+        The th difference between this member and the
+        person who raided them
         th is positive int if th is higher
         th is negative int if th is lower
         """
         self.defending_th_difference = th
 
     def set_defense_stars(self, stars=int):
+        """
+        Define the stars the opponent got from attacking
+        this member's base
+        """
         self.defense_stars = stars
 
     def calc_dkp(self):
+        """
+        Calculate the DKP score for this clan member
+        """
         self.dkp = 0
 
         if self.attack_stars == 3 and self.attacking_th_difference == 0:
@@ -110,7 +124,7 @@ class Member():
             self.dkp += 100
 
         ###
-        
+
         if self.defense_stars == 3 and self.defending_th_difference == 0:
             self.dkp += 100
         elif self.defense_stars == 3 and self.defending_th_difference == 1:
@@ -155,9 +169,11 @@ class Member():
         elif self.defense_stars == 0 and self.defending_th_difference == -2:
             self.dkp += 100
 
-
-
     def make_dict(self):
+        """
+        Creates the dictionary used to write
+        to the csv file
+        """
         self.data = {
             "PlayerName":self.name, 
             "StarsFromAttack":self.attack_stars, 
@@ -168,20 +184,31 @@ class Member():
             }
 
 
-columns = ["PlayerName","StarsFromAttack","THDiffAttack","StarsDefense","THDiffDefense","DKP"]
+# Get all column names
+with open("daily_cwl_data.csv", "r") as f:
+    reader = csv.reader(f)
+    columns = next(reader)
 
+# Open file
 file = open("daily_cwl_data.csv", "a", newline="")
+
+# Create writer
 writer = csv.DictWriter(file, fieldnames=columns)
 
+# For every clan member
 for name in all_members:
+
+    # Create player instance
     player = Member(name)
 
+    # Show the current member
     print(f"\nCurrent member is {player.name}")
 
     # Define stars gained from attack
     while True:
         stars = input("Stars from attack: ")
 
+        # Ensure input is an int and is a value between 0 and 3
         if stars.isnumeric() == True and int(stars) in [0,1,2,3]:
             player.set_attack_stars(int(stars))
             break
@@ -193,6 +220,7 @@ for name in all_members:
     while True:
         opponent_th = input(f"Attacking TH difference: ")
 
+        # Ensure input is an int
         try:
             player.set_attacking_th_difference(int(opponent_th))
             break
@@ -204,6 +232,7 @@ for name in all_members:
     while True:
         stars = input("How many stars did the attacker get from your base: ")
 
+        # Ensure input is an int and is a value between 0 and 3
         if stars.isnumeric() == True and int(stars) in [0,1,2,3]:
             player.set_defense_stars(int(stars))
             break
@@ -214,15 +243,21 @@ for name in all_members:
     while True:
         opponent_th = input(f"Defending TH difference: ")
 
+        # Ensure input is an int
         try:
             player.set_defending_th_difference(int(opponent_th))
             break
         except:
             print("Input must be a positive or negative integer\n")
 
+    # Calculate dkp
     player.calc_dkp()
+
+    # Make dictionary to insert to csv file
     player.make_dict()
 
+    # If the write fails print unknown error
+    # Because I literally dont know what error it could be
     try:
         writer.writerow(player.data)
         print(f"Writing data: {player.data}")
